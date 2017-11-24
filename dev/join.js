ef714520cac08e9a35b1615fe154e4e625578660
John Peter Julius <johnpj1@a80201-la006.org.aalto.fi>
2017-11-24 11:01:46 +0200

    included join.js file + changed audience.js to load join.js script

--- dev/join.js
+++ dev/join.js
@@ -1,9 +1,45 @@
 
 let connection = null;
 let room = null;
+let localTracks = [];
 const remoteTracks = {};
 const changeList = {};
 let frameArray = [1,2,3,4];
+let isJoined = false;
+
+/**
+ * Handles local tracks.
+ * @param tracks Array with JitsiTrack objects
+ */
+function onLocalTracks(tracks) {
+    localTracks = tracks;
+    for (let i = 0; i < localTracks.length; i++) {
+        const participant = localTracks[i].getParticipantId();
+        if (!(participant in changeList)){
+          changeList[participant] = frameArray.shift();
+          console.log('INFO: Participants: ' + JSON.stringify(changeList));
+        }
+
+        var remoteVideo = "#remoteVideo" +changeList[participant];
+        var remoteAudio = "#remoteAudio" +changeList[participant];
+
+        if (localTracks[i].getType() === 'video') {
+            // $('body').append(`<video autoplay='1' id='localVideo${i}' />`);
+            $(remoteVideo).replaceWith(
+              `<div id='remoteVideo${changeList[participant]}'><video autoplay='1' id='localVideo${i}' width='300px'/></div>`);
+            localTracks[i].attach($(`#localVideo${i}`)[0]);
+        } else {
+            // $('body').append(
+                // `<audio autoplay='1' muted='true' id='localAudio${i}' />`);
+            $(remoteAudio).replaceWith(
+                `<div id='remoteAudio${changeList[participant]}'><audio autoplay='1' muted='true' id='localAudio${i}' /></div>`);
+            localTracks[i].attach($(`#localAudio${i}`)[0]);
+        }
+        if (isJoined) {
+            room.addTrack(localTracks[i]);
+        }
+    }
+}
 
 /**
  * Handles remote tracks
@@ -51,11 +87,11 @@ function onRemoteTrackRemove(track) {
     const participant = track.getParticipantId();
     if (participant in changeList) {
       var remoteVideo = "#remoteVideo" +changeList[participant];
-      var remoteAudio = "#remoteAudio" +changeList[participant];	    
+      var remoteAudio = "#remoteAudio" +changeList[participant];
       $(remoteVideo).replaceWith(
           `<div id='remoteVideo${changeList[participant]}'><img src="resources/user.png" width="200px" height="175px"/></div>`);
       $(remoteAudio).replaceWith(
-          `<div id='remoteAudio${changeList[participant]}'></div>`);	      
+          `<div id='remoteAudio${changeList[participant]}'></div>`);
       frameArray.push(changeList[participant]);
       delete changeList[participant];
     }
@@ -63,10 +99,14 @@ function onRemoteTrackRemove(track) {
 }
 
 /**
- * function is executed when the conference is joined
+ * That function is executed when the conference is joined
  */
 function onConferenceJoined() {
-    console.log('INFO: Conference joined in silence!');
+    console.log('INFO: Conference joined!');
+    isJoined = true;
+    for (let i = 0; i < localTracks.length; i++) {
+        room.addTrack(localTracks[i]);
+    }
 }
 
 /**
@@ -133,23 +173,15 @@ JitsiMeetJS.init(config)
             disconnect);
 
         connection.connect();
+        JitsiMeetJS.createLocalTracks({ devices: [ 'audio', 'video' ] })
+            .then(onLocalTracks)
+            .catch(error => {
+                throw error;
+            });
     })
     .catch(error => console.log('ERROR: JitsiMeetJS.init says ' +error));
 
 function btnClick() {
-    if (frameArray.length > 0) {
-        // alert('User may join. Speakers count: ' + (4-frameArray.length));
-        unload();
-	$('script').each(function() {
-	    if (this.src == 'https://fishbowl.havoc.fi/dev/audience.js' ||
-	        this.src == 'https://fishbowl.havoc.fi/dev/libs/audience-config.js')
-		this.parentNode.removeChild(this);
-	});
-        $("#mainBtn").text("Leave"); 
-	$('body').append('<script src="libs/join-config.js"></script>');
-	$('body').append('<script src="join.js"></script>');
-    }
-    else {
-        alert('User cannot join now. Speakers count: ' + (4-frameArray.length));
-    }
+    alert('INFO: Leave functionality not implemented yet!');
 }
+
